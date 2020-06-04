ffc8cfba55beee927b0802260127e1ed9275cc36
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-04 14:06:35 -0300

    API - Update User model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import { Model, Sequelize } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -7,12 +8,20 @@ class User extends Model {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         provider: Sequelize.BOOLEAN,
       },
       {
         sequelize,
       }
     );
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
