9edea39a1546d570c553b7ab870c4d04a106da79
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-04 18:51:50 -0300

    API - Update migration

--- src/database/migrations/20200604205658-create-products.js
+++ src/database/migrations/20200604205658-create-products.js
@@ -11,7 +11,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      decription: {
+      description: {
         type: Sequelize.STRING,
         allowNull: false,
       },
