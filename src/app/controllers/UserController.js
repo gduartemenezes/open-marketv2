b10181e79f05ad91de5ece575fd5e31e93291ea0
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-04 16:26:34 -0300

    API - Update UserCtrl - add Yup data validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Invalid Data, try again' });
+    }
+
     const { email, password, passwordConfirmation } = req.body;
 
     const userExists = await User.findOne({
@@ -30,6 +41,24 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email().required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Invalid Data, try again' });
+    }
+
     const user = await User.findByPk(req.userId);
     const { email, oldPassword } = req.body;
 
