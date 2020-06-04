83d0212bf102177a6c6d478e9136f2dd453cfb4c
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-04 19:00:22 -0300

    API - Add ProviderCtrl.index

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductController from './app/controllers/ProductController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 
@@ -14,5 +15,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.post('/products', ProductController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
